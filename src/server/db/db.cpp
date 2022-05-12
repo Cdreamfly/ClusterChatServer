f5c9033c3d4144fd845b55b06c3a671de4e3e575
Cdreamfly <2290660986@qq.com>
2022-05-12 18:26:02 +0800

    提交漏掉的db.cpp

---
+++ src/server/db/db.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Cmf on 2022/5/12.
+//
+
+#include "server/db/db.h"
+#include "muduo/base/Logging.h"
+
+static const std::string HOST = "127.0.0.1";
+static const unsigned int PORT = 3306;
+static const std::string USER = "root";
+static const std::string PWD = "cmf.199991";
+static const std::string DB = "chat";
+
+MySQL::MySQL()
+{
+    conn_ = mysql_init(nullptr);
+}
+
+MySQL::~MySQL()
+{
+    if(conn_!= nullptr)
+    {
+        mysql_close(conn_);
+    }
+}
+
+MYSQL *MySQL::GetConnect()
+{
+    return conn_;
+}
+
+bool MySQL::Connect()
+{
+    conn_ = mysql_real_connect(conn_,HOST.c_str(),USER.c_str(),PWD.c_str(),DB.c_str(),PORT, nullptr,0);
+    if(conn_ == nullptr)
+    {
+        LOG_ERROR<<"MySQL Connect ERROR!";
+        return false;
+    }
+    return true;
+}
+
+MYSQL_RES *MySQL::Query(std::string& sql)
+{
+    if(mysql_query(conn_,sql.c_str()) != 0)
+    {
+        LOG_INFO<<__FILE__<<":"<<__LINE__<<":"<<sql<<"查询失败:"<< mysql_error(conn_);
+        return nullptr;
+    }
+    return mysql_use_result(conn_);
+}
+
+bool MySQL::Update(std::string &sql)
+{
+    if(mysql_query(conn_,sql.c_str()) != 0)
+    {
+        LOG_INFO<<__FILE__<<":"<<__LINE__<<":"<<sql<<"更新失败:"<< mysql_error(conn_);
+        return false;
+    }
+    return true;
+}
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,6 +7,7 @@
 int main() {
     MySQL sql;
     std::cout<< sql.Connect()<<" hello"<<std::endl;
+
     muduo::net::EventLoop loop;
     muduo::net::InetAddress addr("127.0.0.1",9190);
     ChatServer server(&loop,addr,"ChatServer");
