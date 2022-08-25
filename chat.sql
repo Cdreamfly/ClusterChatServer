435a741aebebdaa6fcc4f3f4eebaca1c1b323710
Cdreamfly <2290660986@qq.com>
2022-08-25 12:50:58 +0800

    有个字段名错了，改了一下

--- chat.sql
+++ chat.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
 --
 -- Host: localhost    Database: chat
 -- ------------------------------------------------------
--- Server version	8.0.29-0ubuntu0.20.04.3
+-- Server version	8.0.30-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,21 +24,11 @@ DROP TABLE IF EXISTS `allgroup`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `allgroup` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `groupame` varchar(50) NOT NULL,
+  `groupname` varchar(50) NOT NULL,
   `groupdesc` varchar(200) DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `groupame` (`groupame`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `allgroup`
---
-
-LOCK TABLES `allgroup` WRITE;
-/*!40000 ALTER TABLE `allgroup` DISABLE KEYS */;
-/*!40000 ALTER TABLE `allgroup` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `friend`
@@ -53,15 +43,6 @@ CREATE TABLE `friend` (
   PRIMARY KEY (`userid`,`friendid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `friend`
---
-
-LOCK TABLES `friend` WRITE;
-/*!40000 ALTER TABLE `friend` DISABLE KEYS */;
-/*!40000 ALTER TABLE `friend` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `groupuser`
@@ -79,15 +60,6 @@ CREATE TABLE `groupuser` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `groupuser`
---
-
-LOCK TABLES `groupuser` WRITE;
-/*!40000 ALTER TABLE `groupuser` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groupuser` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `offlinemessage`
 --
 
@@ -99,15 +71,6 @@ CREATE TABLE `offlinemessage` (
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `offlinemessage`
---
-
-LOCK TABLES `offlinemessage` WRITE;
-/*!40000 ALTER TABLE `offlinemessage` DISABLE KEYS */;
-/*!40000 ALTER TABLE `offlinemessage` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -123,18 +86,8 @@ CREATE TABLE `user` (
   `state` enum('online','offline') DEFAULT 'offline',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'cmf','123456','online'),(2,'ccc','123456','online');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -145,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-13 21:18:55
+-- Dump completed on 2022-08-25 12:49:37
